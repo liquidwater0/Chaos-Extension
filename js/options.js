336734e4b6ca18f590729c8f7d439af37d459500
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2021-06-17 19:06:50 -0500

    added new effect timer to options

--- js/options.js
+++ js/options.js
@@ -41,21 +41,32 @@ const rainbowTextEffectCheckbox = document.getElementById("rainbowTextEffectChec
 const flipPageEffectCheckbox = document.getElementById("flipPageEffectCheckbox");
 const disableEverythingEffectCheckbox = document.getElementById("disableEverythingEffectCheckbox");
 const removeScrollbarsEffectCheckbox = document.getElementById("removeScrollbarsEffectCheckbox");
+const newEffectTimerTextField = document.getElementById("newEffectTimerTextField");
 
 function save() {
     chrome.storage.sync.set({
         "nothingEffectChecked": nothingEffectCheckbox.checked, "rainbowTextEffectChecked": rainbowTextEffectCheckbox.checked, "flipPageEffectChecked": flipPageEffectCheckbox.checked,
-        "disableEverythingEffectChecked": disableEverythingEffectCheckbox.checked, "removeScrollbarsEffectChecked": removeScrollbarsEffectCheckbox.checked
+        "disableEverythingEffectChecked": disableEverythingEffectCheckbox.checked, "removeScrollbarsEffectChecked": removeScrollbarsEffectCheckbox.checked, 
+        "newEffectTimer": newEffectTimerTextField.value.split(" ")[0]
     }, function() {
         saveButton.textContent = "Saved!";
         setTimeout(function() { saveButton.textContent = "Save" }, 3000);
+
+        chrome.storage.sync.get({ newEffectTimer: 10 }, function(items) {
+            if (items.newEffectTimer == "") {
+                newEffectTimerTextField.value = 10;
+                save();
+            } else {
+                newEffectTimerTextField.value = (items.newEffectTimer == 1) ? `${items.newEffectTimer} second` : `${items.newEffectTimer} seconds`;
+            }
+        })
     });
 }
 
 function get() {
     chrome.storage.sync.get({
         currentTheme: "dark", themeSwitchChecked: true, nothingEffectChecked: true, rainbowTextEffectChecked: true, flipPageEffectChecked: true, disableEverythingEffectChecked: true,
-        removeScrollbarsEffectChecked: true
+        removeScrollbarsEffectChecked: true, newEffectTimer: 10
     }, function(items) {
         html.setAttribute("data-theme", items.currentTheme);
         themeSwitch.checked = items.themeSwitchChecked;
@@ -64,5 +75,6 @@ function get() {
         flipPageEffectCheckbox.checked = items.flipPageEffectChecked;
         disableEverythingEffectCheckbox.checked = items.disableEverythingEffectChecked;
         removeScrollbarsEffectCheckbox.checked = items.removeScrollbarsEffectChecked;
+        newEffectTimerTextField.value = (items.newEffectTimer == 1) ? `${items.newEffectTimer} second` : `${items.newEffectTimer} seconds`;
     });
 }
