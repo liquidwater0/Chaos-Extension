db0b8a9e532371054eda9ef9418716f23af14b1a
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2021-06-17 19:08:34 -0500

    modified code to use new effect timer in options

--- js/chaos.js
+++ js/chaos.js
@@ -13,52 +13,56 @@ document.body.insertAdjacentHTML("afterbegin", `
   </div>
 `);
 
-const timer = 5;
-let timerSeconds = timer;
+let timer;
 
-const timeRemaining = document.querySelector("[data-extension='chaosExtension'] #timeRemaining");
-timeRemaining.textContent = timerSeconds;
+chrome.storage.sync.get({ newEffectTimer: 10 }, function(items) {
+  timer = items.newEffectTimer;
+  let timerSeconds = timer;
 
-let timerPause = false;
+  const timeRemaining = document.querySelector("[data-extension='chaosExtension'] #timeRemaining");
+  timeRemaining.textContent = timerSeconds;
 
-document.addEventListener("keydown", function(event) {
-  if (event.shiftKey && event.keyCode == 80) { //Shift + P (80)
-    timerPause = !timerPause;
+  let timerPause = false;
+
+  document.addEventListener("keydown", function(event) {
+    if (event.shiftKey && event.keyCode == 80) { //Shift + P (80)
+      timerPause = !timerPause;
+    }
+  });
+
+  setInterval(updateTimerBar, 1000);
+
+  function updateTimerBar() {
+    const timerBar = document.querySelector("[data-extension='chaosExtension'] #timerBar");
+    const timerBarWidth = Number(window.getComputedStyle(timerBar, null).getPropertyValue("width").replace("px", ""));
+
+    if (!timerPause) {
+      timerBar.style.width = `${timerBarWidth - (timerBarWidth / 100 * (100 / timerSeconds--))}px`;
+      timeRemaining.textContent = timerSeconds;
+    }
+
+    if (timerSeconds < 0) {
+      timerBar.style.width = "100%";
+      timerSeconds = timer;
+      timeRemaining.textContent = timer;
+      newEffect();
+    }
+  }
+
+  function newEffect() {
+    const enabledEffects = effects.filter(function(effect) { return effect.enabled == true });
+    const randomEffect = enabledEffects[Math.floor(Math.random() * enabledEffects.length)];
+
+    const effectContainer = document.querySelector("[data-extension='chaosExtension'] #effectContainer");
+    const effectName = document.querySelector("[data-extension='chaosExtension'] #effectName");
+    
+    enabledEffects.forEach(function(enabledEffect) { enabledEffect.setDefaultValues() });
+
+    if (randomEffect) {
+      effectContainer.style.display = "block";
+
+      effectName.textContent = randomEffect.name;
+      randomEffect.effectCode();
+    }
   }
 });
-
-setInterval(updateTimerBar, 1000);
-
-function updateTimerBar() {
-  const timerBar = document.querySelector("[data-extension='chaosExtension'] #timerBar");
-  const timerBarWidth = Number(window.getComputedStyle(timerBar, null).getPropertyValue("width").replace("px", ""));
-
-  if (!timerPause) {
-    timerBar.style.width = `${timerBarWidth - (timerBarWidth / 100 * (100 / timerSeconds--))}px`;
-    timeRemaining.textContent = timerSeconds;
-  }
-
-  if (timerSeconds < 0) {
-    timerBar.style.width = "100%";
-    timerSeconds = timer;
-    timeRemaining.textContent = timer;
-    newEffect();
-  }
-}
-
-function newEffect() {
-  const enabledEffects = effects.filter(function(effect) { return effect.enabled == true });
-  const randomEffect = enabledEffects[Math.floor(Math.random() * enabledEffects.length)];
-
-  const effectContainer = document.querySelector("[data-extension='chaosExtension'] #effectContainer");
-  const effectName = document.querySelector("[data-extension='chaosExtension'] #effectName");
-  
-  enabledEffects.forEach(function(enabledEffect) { enabledEffect.setDefaultValues() });
-
-  if (randomEffect) {
-    effectContainer.style.display = "block";
-
-    effectName.textContent = randomEffect.name;
-    randomEffect.effectCode();
-  }
-}
