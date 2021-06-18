9225e2d197f0618be1c924c809e238a0b3801a8f
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2021-06-17 23:02:54 -0500

    save check state for the 2 new checkboxes

--- js/options.js
+++ js/options.js
@@ -42,12 +42,14 @@ const flipPageEffectCheckbox = document.getElementById("flipPageEffectCheckbox")
 const disableEverythingEffectCheckbox = document.getElementById("disableEverythingEffectCheckbox");
 const removeScrollbarsEffectCheckbox = document.getElementById("removeScrollbarsEffectCheckbox");
 const newEffectTimerTextField = document.getElementById("newEffectTimerTextField");
+const reloadPageEffectCheckbox = document.getElementById("reloadPageEffectCheckbox");
+const getAlertEffectCheckbox = document.getElementById("getAlertEffectCheckbox");
 
 function save() {
     chrome.storage.sync.set({
         "nothingEffectChecked": nothingEffectCheckbox.checked, "rainbowTextEffectChecked": rainbowTextEffectCheckbox.checked, "flipPageEffectChecked": flipPageEffectCheckbox.checked,
         "disableEverythingEffectChecked": disableEverythingEffectCheckbox.checked, "removeScrollbarsEffectChecked": removeScrollbarsEffectCheckbox.checked, 
-        "newEffectTimer": newEffectTimerTextField.value.split(" ")[0]
+        "newEffectTimer": newEffectTimerTextField.value.split(" ")[0], "reloadPageEffectChecked": reloadPageEffectCheckbox.checked, "getAlertEffectChecked": getAlertEffectCheckbox.checked
     }, function() {
         saveButton.textContent = "Saved!";
         setTimeout(function() { saveButton.textContent = "Save" }, 3000);
@@ -66,7 +68,7 @@ function save() {
 function get() {
     chrome.storage.sync.get({
         currentTheme: "dark", themeSwitchChecked: true, nothingEffectChecked: true, rainbowTextEffectChecked: true, flipPageEffectChecked: true, disableEverythingEffectChecked: true,
-        removeScrollbarsEffectChecked: true, newEffectTimer: 10
+        removeScrollbarsEffectChecked: true, newEffectTimer: 10, reloadPageEffectChecked: true, getAlertEffectChecked: true
     }, function(items) {
         html.setAttribute("data-theme", items.currentTheme);
         themeSwitch.checked = items.themeSwitchChecked;
@@ -76,5 +78,7 @@ function get() {
         disableEverythingEffectCheckbox.checked = items.disableEverythingEffectChecked;
         removeScrollbarsEffectCheckbox.checked = items.removeScrollbarsEffectChecked;
         newEffectTimerTextField.value = (items.newEffectTimer == 1) ? `${items.newEffectTimer} second` : `${items.newEffectTimer} seconds`;
+        reloadPageEffectCheckbox.checked = items.reloadPageEffectChecked;
+        getAlertEffectCheckbox.checked = items.getAlertEffectChecked;
     });
 }
