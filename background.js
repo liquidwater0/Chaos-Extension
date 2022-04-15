19d881c066784080ba54645be1fc6dcccf4247e0
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2022-04-15 01:15:57 -0500

    indents....

--- background.js
+++ background.js
@@ -2,8 +2,8 @@ chrome.runtime.onInstalled.addListener(details => {
     if (details.reason !== "install") return;
 
     if (chrome.runtime.openOptionsPage) {
-		chrome.runtime.openOptionsPage();
-	} else {
-		window.open(chrome.runtime.getURL("options.html"));
-	}
+	chrome.runtime.openOptionsPage();
+    } else {
+	window.open(chrome.runtime.getURL("options.html"));
+    }
 });
