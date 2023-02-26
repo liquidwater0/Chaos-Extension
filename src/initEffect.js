170cb46a30df1f4502955f152dca26c7b7118a7e
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2023-02-25 19:38:32 -0600

    always set enabled to the default enabled state

--- src/initEffect.js
+++ src/initEffect.js
@@ -1,19 +1,14 @@
-import getEnabled from "./utitilies/getEnabled";
-
 export const effectsMap = new Map([]);
 
 export default function initEffect({ effectName, label, storageKey, defaultEnabled, activate, revert }) {
-    getEnabled(storageKey).then(checkedState => {
-        // console.log(checkedState, defaultEnabled);
-        effectsMap.set(
-            effectName, 
-            { 
-                enabled: checkedState || defaultEnabled,
-                label, 
-                storageKey, 
-                activate, 
-                revert
-            }
-        );
-    });
+    effectsMap.set(
+        effectName, 
+        { 
+            enabled: defaultEnabled,
+            label, 
+            storageKey, 
+            activate, 
+            revert
+        }
+    );
 }
