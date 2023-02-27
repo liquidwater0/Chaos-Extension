afdcf7d388a763413eaea5d66773ac7004e8e9b7
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2023-02-27 14:02:31 -0600

    put ActiveEffects component in ChaosUI directly

--- src/pages/content/ChaosUI.jsx
+++ src/pages/content/ChaosUI.jsx
@@ -2,12 +2,13 @@ import React, { useEffect } from 'react';
 import "./scss/content.scss";
 import { useChromeStorageSync } from 'use-chrome-storage';
 import { initialTheme } from '../../initialOptions';
+import { useChaosEffects } from './context/EffectsContext';
 import Font from "../../global components/Font";
 import Timer from './components/Timer';
-import ActiveEffects from './components/ActiveEffects';
 
 export default function ChaosUI() {
     const [theme] = useChromeStorageSync("theme", initialTheme);
+    const { activeEffect } = useChaosEffects();
 
     useEffect(() => {
         document.documentElement.setAttribute("data-extension", "chaosExtension");
@@ -17,7 +18,13 @@ export default function ChaosUI() {
         <div data-theme={theme}>
             <Font/>
             <Timer/>
-            <ActiveEffects/>
+            
+            <div className="active-effects ui-blur">
+                <div className='label'>Effect Activated</div>
+                <div className={`active-effect-text ${activeEffect === "" ? "hide-text" : ""}`}>
+                    { activeEffect === "" ? "a" : activeEffect }
+                </div>
+            </div>
         </div>
     );
 }
