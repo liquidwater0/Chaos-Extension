aed7df353c58aa757d5f4503c1351d212e3897bf
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2023-03-05 21:12:24 -0600

    handle new and removed effects

--- src/pages/background/index.ts
+++ src/pages/background/index.ts
@@ -1,23 +1,19 @@
 import { initialTimerSeconds, initialTimerOptions } from "../../initialOptions";
 import "../../effects/index";
 import { effects } from "../../Effect";
+import { EffectOption } from "../../types";
 
 const initialEffectOptions = effects.map(({ label, storageKey, enabled }) => {
 	return { label, storageKey, enabled };
 });
 
-function saveStorage() {
-	chrome.storage.sync.set({
-		checkedStates: initialEffectOptions,
-		timer: initialTimerSeconds,
-		timerOptions: initialTimerOptions
-	});
-}
-
 chrome.runtime.onInstalled.addListener(details => {
     if (details.reason !== "install") return;
 
-	saveStorage();
+	chrome.storage.sync.set({
+		timer: initialTimerSeconds,
+		timerOptions: initialTimerOptions
+	});
 
     if (chrome.runtime.openOptionsPage) {
 		chrome.runtime.openOptionsPage();
@@ -25,3 +21,31 @@ chrome.runtime.onInstalled.addListener(details => {
 		window.open(chrome.runtime.getURL("options.html"));
 	}
 });
+
+handleUpdatedEffects();
+
+function handleUpdatedEffects() {
+	chrome.storage.sync.get({ checkedStates: initialEffectOptions }, items => {
+		let finalArray: EffectOption[] = items.checkedStates;
+
+		initialEffectOptions.forEach(effect => {
+			const newEffect: EffectOption | undefined = items.checkedStates.find((item: EffectOption) => {
+				return item.storageKey === effect.storageKey;
+			});
+
+			if (newEffect === undefined) finalArray.push(effect);
+		});
+
+		items.checkedStates.forEach((effect: EffectOption) => {
+			const removedEffect: EffectOption | undefined = initialEffectOptions.find((item: EffectOption) => {
+				return item.storageKey === effect.storageKey;
+			});
+
+			if (removedEffect === undefined) {
+				finalArray = finalArray.filter(item => item.storageKey !== effect.storageKey);
+			}
+		});
+
+		chrome.storage.sync.set({ checkedStates: finalArray });
+	});
+}
