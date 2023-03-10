01709b8a625dc85d414e2e65a6e887c9cb83af86
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2023-03-09 22:53:53 -0600

    fix roboto font not working

--- src/pages/content/index.tsx
+++ src/pages/content/index.tsx
@@ -7,6 +7,13 @@ const rootElement = document.createElement("div");
 rootElement.id = "chaosExtensionRoot";
 rootElement.setAttribute("data-extension", "chaosExtension");
 
+//why do i have to do this?
+const fontLink = document.createElement("link");
+fontLink.rel = "stylesheet";
+fontLink.href = "https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap";
+
+rootElement.appendChild(fontLink);
+
 const link = document.createElement("link");
 link.rel = "stylesheet";
 link.href = chrome.runtime.getURL("content/content.css");
