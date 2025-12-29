7d7ef1dc987379d5ed3556e9274375ec3664e338
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2025-12-29 01:12:35 -0600

    inverted colors ui theme

--- src/effects/invertedColorsEffect/index.ts
+++ src/effects/invertedColorsEffect/index.ts
@@ -7,16 +7,23 @@ export default new Effect({
     id: "invertedColorsEffect",
     label: "Inverted Colors",
     durationMs: DEFAULT_EFFECT_DURATION_MS,
-    tags: [TAGS.OVERLAY],
+    tags: [
+        TAGS.OVERLAY, 
+        TAGS.THEME_BACKGROUND, 
+        TAGS.THEME_TEXT_COLOR
+    ],
     activate,
     revert
 });
 
 function activate() {
+    document.documentElement.classList.add("inverted-colors-effect");
     new Overlay({ id: "invertOverlay", pointerEvents: false });
 }
 
 function revert() {
+    document.documentElement.classList.remove("inverted-colors-effect");
+
     const invertOverlay = document.querySelector("[chaos-extension-overlay]#invertOverlay");
     if (!invertOverlay) return;
     invertOverlay.remove();
