b500eaed62a8a954b792826c33c91fc342f3e9d2
liquidwater0 <32186774+liquidwater0@users.noreply.github.com>
2025-12-29 01:02:49 -0600

    1950s ui theme

--- src/effects/y1950sEffect/index.ts
+++ src/effects/y1950sEffect/index.ts
@@ -7,16 +7,19 @@ export default new Effect({
     id: "y1950sEffect",
     label: "1950s",
     durationMs: DEFAULT_EFFECT_DURATION_MS,
-    tags: [TAGS.OVERLAY],
+    tags: [TAGS.OVERLAY, TAGS.THEME_BACKGROUND],
     activate,
     revert
 });
 
 function activate() {
+    document.documentElement.classList.add("y1950s-effect");
     new Overlay({ id: "y1950sOverlay", pointerEvents: false });
 }
 
 function revert() {
+    document.documentElement.classList.remove("y1950s-effect");
+
     const y1950sOverlay = document.querySelector("[chaos-extension-overlay]#y1950sOverlay");
     if (!y1950sOverlay) return;
     y1950sOverlay.remove();
